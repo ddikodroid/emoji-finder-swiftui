e19746e38f33e1e9d4936bef368fe3fd1fc0df21
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-11-18 21:01:45 +0700

    Initial Commit

---
+++ EmojiFinder/EmojiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiFinderApp.swift
+//  EmojiFinder
+//
+//  Created by Ahmad Syarifuddin Randiko on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
